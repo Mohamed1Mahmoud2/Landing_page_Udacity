9c12539cd8bca2347a6cade0fe4087be566296b8
Mohamed Mahmoud <103222555+Mohamed1Mahmoud2@users.noreply.github.com>
2022-10-20 03:50:00 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const listNav = document.getElementById("navbar__list");
+const sections = [...document.querySelectorAll("section")]
+let navListContainer = '';
+
+/**
+ * make list items 
+ */
+
+function navUlList() {
+    sections.forEach(section => {
+        const sectionID = section.id;
+        const sectionAtrribute = section.dataset.nav;
+        navListContainer += `<li> <a class="list_sec" data-nav="${sectionID}" href="#${sectionID}">${sectionAtrribute}</a></li>`
+    })
+    listNav.innerHTML = navListContainer;
+}
+navUlList();
+
+/** when click on nav links && animation */
+
+listNav.addEventListener("click", () => { document.scrollIntoView({ behavior: "smooth" }) });
+
+/**animation to sections and nav bar <a>link  highlight*/
+
+window.onscroll = function () {
+    sections.forEach((element) => {
+        let navActv = listNav.querySelector(`[data-nav=${element.id}]`);
+        if (element.getBoundingClientRect().top <= 110 && element.getBoundingClientRect().top >= -520) {
+            element.classList.add("your-active-class");
+            navActv.classList.add("navSec-active");
+        } else {
+            element.classList.remove("your-active-class");
+            navActv.classList.remove("navSec-active");
+        }
+    });
+}
+/*
+test element position
+*/
+// console.log(document.getElementById("section1").getBoundingClientRect());
+// console.log(document.getElementById("section2").getBoundingClientRect());
+// console.log(document.getElementById("section3").getBoundingClientRect());
+//sction h529
